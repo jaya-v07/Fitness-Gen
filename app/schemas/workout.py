0a1bea507023f8ee3e7c6b789265e1694a46156b
Jaya Vanishitta <j.vanishitta@gmail.com>
2026-06-06 18:24:33 +0530

    fix: changed data type to float

--- app/schemas/workout.py
+++ app/schemas/workout.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class WorkoutRequest(BaseModel):
     user_id: str
     age: int
-    weight: int
-    height: int
+    weight: float
+    height: float
     goal: str
     experience: str
     
